679f78d0cc36ee663a6db1dde3fc8d3da772c9fa
Valentin <valentinwissler42@outlook.com>
2022-11-26 20:10:32 +0000

    Passing all required tests

--- index.js
+++ index.js
@@ -31,8 +31,7 @@ const urlSchema = new mongoose.Schema({
 //creates a model from schema, mdels are fancy constructors 
 let URL = mongoose.model('URL', urlSchema);
 
-//create one that deletes
-//create one that queries by short-url
+
 //create one that queries by url
 function retrieveURLId(url, done) {
   URL.findOne({url: url}, (err, data) => {
@@ -49,8 +48,10 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+//serves the css file
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+//serves the HTML file
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -59,6 +60,23 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+// Display time
+app.get('/api/now', function(req, res) {
+  res.json({ time: new Date()});
+});
+
+//route parameters
+app.get('/:word/echo', (req, res) => {
+  res.json({echo: req.params.word});
+})
+
+//more complex route parameters
+app.get('/user/:userName/level/:userLevel', (req, res) => {
+  res.json({Greeting: `Hi ${req.params.userName} it's nice to see you again with your massive level ${req.params.userLevel} :)`});
+})
+
+
 
 //create a function that create and save a document
 function creatAndSaveURL(object, done) {
@@ -87,74 +105,95 @@ function fetchAll(done) {
   });
 }
 
-function findByURL(url, done) {
-  URL.find({url: url}, (err, data) => {
+app.get('/api/shorturl/:short_url', (req, res) => {
+  console.log(req.params.short_url);
+  findByShortURL(req.params.short_url, (err, data) => {
     if(err) return console.error(err);
-    if (!data) {
-      console.log("Missing `done()` argument");
-      return console.error({ message: "Missing callback argument" });
+    //the url returned is data[0]["url"]
+    res.redirect(data[0]["url"]);
+  });
+})
+
+function findByURL(object, done) {
+  //Model.exists returns an object if a result is found
+  //otherwise it returns null
+  URL.exists({url: object.url}, (err, result) => {
+    if(err) return console.error(err);
+    else {
+      done(null, result);
     }
-    done(null, data);
+  });
+}
+
+function findByShortURL(short_url, done) {
+  //Model.find returns an array of object
+  //here we target via unique (they should be unique.... but they aren;t and
+  //duplicate may exist, multiple results will be returned but only
+  //the first will be used with [0]["url"]
+  URL.find({short_url: short_url}, (err, result) => {
+    if(err) return console.error(err);
+    else {
+      done(null, result);
+    }
   });
 }
 
 app.post("/api/shorturl", (req, res) => {
-  let count = 0;
-  let index;
 
-  fetchAll((err, arr) => {
-    if(err) return console.error(err);
-    if (!arr) {
-      console.log("Missing `done()` argument");
-      return console.error({ message: "Missing callback argument" });
-    }
-    index = arr;
-    count = arr.length;
-    console.log(`There are ${count} documents in the table URLs`);
-  })
+  let URLvalidator = new RegExp('^https?:\/\/');
+  const fullURL = req.body.url;
+  const protocolLessURL = fullURL.replace(URLvalidator, '');
+  console.log(fullURL);
+  console.log(protocolLessURL);
+  
+  // //clean slate 
+  // clearDB();
 
-  findByURL(req.body.url, (err, data) => {
-    if(err) return next(err);
-    if (!data) {
-      console.log("Missing `done()` argument");
-      return console.error({ message: "Missing callback argument" });
-    }
-    console.log("Entry found for req.body.url: " + data);
-  })
-
-  //url in req body is the one sent through the form 
-  creatAndSaveURL({url: req.body.url, short_url: count + 1}, (err, url) => {
-    if(err) return nconsole.error(err);
-    if (!url) {
-      console.log("Missing `done()` argument");
-      return console.error({ message: "Missing callback argument" });
-    }
-    URL.findById(url._id, function (err, data) {
-      if (err) {
-        return console.error(err);
+  let randomNumber = Math.floor(Math.random() * 100000 + 1);
+  //if the dns name is not resolved
+  if( ! URLvalidator.test(fullURL) ) {
+    res.json({error: 'invalid url'});
+  }
+  else {
+    fetchAll((err, arr) => {
+      if(err) return console.error(err);
+      if (!arr) {
+        console.log("Missing `done()` argument");
+        return console.error({ message: "Missing callback argument" });
       }
-      res.json(data);
-  });
+      console.log(`There are ${arr.length} documents in the table URLs`);
+    })
+
+    findByURL(req.body, (err, data) => {
+      //handle errors before data
+      if(err) return console.error(err);
+      //url in req body is the one sent through the form 
+      creatAndSaveURL({url: req.body.url, short_url: randomNumber}, (err, url) => {
+        if(err) return console.error(err);
+        if (!url) {
+          console.log("Missing `done()` argument");
+          return console.error({ message: "Missing callback argument" });
+        }
+        URL.findById(url._id, function (err, data) {
+          if (err) {
+            return console.error(err);
+          }
+          res.json({original_url: data.url, short_url: data.short_url});
+        });
+      });
+    });
+  }
 });
   //we need to make sure it starts with https://www. || http://www.
   //dns lookup need to have the .com extension and the hostname without the protocol
   //google.com ok | google not ok 
-  // dns.lookup(postData.url, function onLookup(err, address, family) {
-  //   //if the dns name is not resolved
-  //   if(err) console.log(err.stack);
-    
-  //   //if the dns is valid
-  //   else {
-  //     console.log('Valid DNS');
-  //     console.log(URL.find({url: postData.url}, (err, data) => {
-  //       if(err) return console.error(err);
-  //       else {
-  //         return console.log(data);
-  //       }
-  //     }));
-  // });
-});
   
+function clearDB() {
+  URL.deleteMany({url: /\w+/}, (err, result) => {
+    if(err) return console.error(err);
+    console.log(result);
+  });
+}
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
